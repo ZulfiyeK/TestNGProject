32b347ac618f34d0ab7d13a50236940fa9f0e93d
Zulfiye <zulfiyezulfiyek@gmail.com>
2023-07-21 22:20:21 +0300

    C02_DataProviderTest

---
+++ src/test/java/techproed/tests/day28_DataProvider/C02_DataProviderTest.java
@@ -0,0 +1,42 @@
+package techproed.tests.day28_DataProvider;
+
+import org.openqa.selenium.Keys;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import techproed.pages.GooglePage;
+import techproed.utilities.ConfigReader;
+import techproed.utilities.Driver;
+import techproed.utilities.ReusableMethods;
+
+public class C02_DataProviderTest {
+
+
+    /*
+     Google sayfasina gidip
+     DataProvider ile belirtilen arac isimlerini aratalim
+     */
+
+    @DataProvider
+    public static Object[][] arabalar() {
+        return new Object[][]{{"BMV"},{"Wolksvagen"},{"Mercedes"},{"Volvo"},{"Honda"}};
+    }
+
+
+
+    @Test(dataProvider = "arabalar")
+    public void test01(String araclar) {
+        //google sayfasina gidelim
+        Driver.getDriver().get(ConfigReader.getProperty("googleUrl"));
+
+        //Dataprovider'deki verilerle arama yapalim
+        GooglePage googlePage = new GooglePage();
+        googlePage.aramaKutusu.sendKeys(araclar, Keys.ENTER);
+        ReusableMethods.bekle(2);
+
+        //Her arama icin sayfa resmi alalim
+        ReusableMethods.tumSayfaResmi();
+
+
+
+    }
+}
