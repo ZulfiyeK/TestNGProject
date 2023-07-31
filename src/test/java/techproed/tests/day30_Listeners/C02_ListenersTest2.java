7548785db912d3af426f7fec2be282007b8ca2ff
Zulfiye <zulfiyezulfiyek@gmail.com>
2023-08-01 00:57:48 +0400

    day_30

--- src/test/java/techproed/tests/day30_Listeners/C02_ListenersTest2.java
+++ src/test/java/techproed/tests/day30_Listeners/C02_ListenersTest2.java
@@ -1,6 +1,41 @@
 package techproed.tests.day30_Listeners;
 
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.SkipException;
+import org.testng.annotations.Test;
+import techproed.utilities.ConfigReader;
+import techproed.utilities.Driver;
+
 public class C02_ListenersTest2 {
 
+    @Test
+    public void test01() {
+        System.out.println("Passed");
+        Assert.assertTrue(true);
+        Assert.assertEquals(2,2);
+    }
 
+
+
+
+    @Test
+    public void test02() {
+        System.out.println("Fail");
+        Assert.assertTrue(false);
+
+    }
+
+
+    @Test
+    public void test03() {
+        System.out.println("Skip");
+        throw new SkipException("Bu method atlandi");
+    }
+
+    @Test
+    public void test04() {
+        Driver.getDriver().get(ConfigReader.getProperty("amazonUrl"));
+        Driver.getDriver().findElement(By.id("lkdmflndfldsk")).click();
+    }
 }
